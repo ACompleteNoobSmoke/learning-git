135736d351603e17950064549db34998fc487d95
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2021-09-08 17:22:10 -0500

    Edited in VS

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-ECHO is on.
-<!DOCTYPE html>
-<html>
-<head>
-<title>Page Title</title>
-</head>
-<body>
-
-
-<h1>You will learn Japanese</h1>
-<h1>I will be changing this heading</h1>
-<p>I posted this in a JavaScript file what a goofball.</p>
-
-
-</body>
-</html>
+console.log("Hello there")
